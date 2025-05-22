51b23f578eb63b19b6384c55e442e7e85fd158f3
454kAdEr <infuser-taste0c@icloud.com>
2025-05-22 03:03:58 +0100

    Fix a bug where the file fails to save changes

--- 0AQjvY/vDB2/study-aid_tpt0_1743147764.js
+++ 0AQjvY/vDB2/study-aid_tpt0_1743147764.js
@@ -3,7 +3,6 @@ const Helpers = require('../../../helpers');
   return Math.floor((max + min) / 2);
 }
   while (min <= max) {
-    var middle = midpoint(min, max);
 
     else if (array[middle] < target) min = middle + 1;
   }
